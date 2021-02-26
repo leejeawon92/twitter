67e173d6fe1ebf25c6290f63ff295ba50f6c92ff
LeeJeaWon <73148498+LeeJeaWon@users.noreply.github.com>
2021-02-26 10:44:30 +0900

    key setting

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 import firebase from "firebase/app";
 const firebaseConfig = {
-    apiKey: "AIzaSyC6TSQUs2EMSCcJHHO3gkQidXwgEJY_J4s",
-    authDomain: "twitter-c2fc8.firebaseapp.com",
-    projectId: "twitter-c2fc8",
-    storageBucket: "twitter-c2fc8.appspot.com",
-    messagingSenderId: "1020228478313",
-    appId: "1:1020228478313:web:04ec5e3aabb585a663c9f8"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID 
 };
 export default firebase.initializeApp(firebaseConfig);
