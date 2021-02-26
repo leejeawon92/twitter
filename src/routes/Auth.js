64419dff76a90416183b5b922000c4b639a63419
LeeJeaWon <73148498+LeeJeaWon@users.noreply.github.com>
2021-02-26 17:16:24 +0900

    Auth form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Auth = () => <span>Auth </span>
+const Auth = () =>{
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
+    const onChange = (event) => {
+        const {target: {name,value}} = event;
+        if(name ==='email'){ 
+            setEmail(value)
+        }
+        else if( name ==='password'){ 
+            setPassword(value)
+        }
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name='email' placeholder='Email' required value={email} onChange={onChange}></input>
+                <input name='password' placeholder='Password' required value={password} onChange={onChange}></input>
+                <input type='submit' placeholder='Log In' required></input>
+            </form>
+            <div>
+                <button>Continue With Google</button>
+                <button>Continue With Github</button>
+            </div>
+        </div>
+    )
+}
 export default Auth
